eaa5f2935c99eb10f9d7222da201f9aadea8ad3e
zirconias <islam.drissi@gmail.com>
2026-04-16 20:58:22 +0200

    wip on miroyoku

---
+++ miryoku/custom_config.h
@@ -0,0 +1,9 @@
+// Copyright 2021 Manna Harbour
+// https://github.com/manna-harbour/miryoku
+
+// Miryoku configuration for Corne Choc Pro BT with QWERTY layout
+
+#define MIRYOKU_ALPHAS_QWERTY
+#define MIRYOKU_TAP_QWERTY
+#define MIRYOKU_EXTRA_COLEMAKDH
+#define MIRYOKU_CLIPBOARD_MAC
